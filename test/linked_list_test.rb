6d0f5c6cb8ed2565a300d8e9d736c5382459621c
Chris M <chris@adadevelopersacademy.org>
2019-08-15 08:17:32 -0700

    Adding comments

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -46,21 +46,24 @@ describe LinkedList do
         end
 
         it 'will return `nil` for `getFirst` if the list is empty' do
-
+            # Act-Assert
             expect(@list.get_first).must_be_nil
         end
     end
 
     describe "length" do
         it "will return 0 for an empty list" do
+            # Act-Assert
             expect(@list.length).must_equal 0
         end
 
         it "will return the length for nonempty lists" do
+            # Arrange
             count = 0
             while count < 5
                 @list.add_first(count)
                 count += 1
+                # Act-Assert
                 expect(@list.length).must_equal count
             end
         end
@@ -68,16 +71,22 @@ describe LinkedList do
 
     describe "addLast & getLast" do
         it "will add to the front if the list is empty" do
+            # Arrange
             @list.add_last(1)
+            # Act-Assert
             expect(@list.get_first).must_equal 1
         end
 
         it "will put new items to the rear of the list" do
+            # Arrange
             @list.add_last(2)
+            # Act-Assert
             expect(@list.length).must_equal 1
             expect(@list.get_last).must_equal 2
 
+            # Arrange
             @list.add_last(3)
+            # Act-Assert
             expect(@list.get_first).must_equal 2
             expect(@list.get_last).must_equal 3
             expect(@list.length).must_equal 2
@@ -91,15 +100,18 @@ describe LinkedList do
 
     describe 'get_at_index' do
         it 'returns nil if the index is outside the bounds of the list' do
+            # Act-Assert
             expect(@list.get_at_index(3)).must_be_nil
         end
 
         it 'can retrieve an item at an index in the list' do
+            # Arrange
             @list.add_first(1)
             @list.add_first(2)
             @list.add_first(3)
             @list.add_first(4)
 
+            # Act-Assert
             expect(@list.get_at_index(0)).must_equal 4
             expect(@list.get_at_index(1)).must_equal 3
             expect(@list.get_at_index(2)).must_equal 2
@@ -109,11 +121,13 @@ describe LinkedList do
 
     describe 'max and min values' do
         it 'returns nil if the list is empty' do
+            # Act-Assert
             expect(@list.find_max()).must_be_nil
             expect(@list.find_min()).must_be_nil
         end
 
         it 'can retrieve the max and min values in the list' do
+            # Arrange
             count = 0
             while count < 5
                 @list.add_first(count)
@@ -121,9 +135,10 @@ describe LinkedList do
                 expect(@list.find_min).must_equal 0
                 count += 1
             end
-
             @list.add_last(100)
             @list.add_first(-12)
+            
+            # Act-Assert
             expect(@list.find_max).must_equal 100
             expect(@list.find_min).must_equal(-12)
         end
@@ -131,28 +146,37 @@ describe LinkedList do
 
     describe "delete" do
         it "delete from empty linked list is a no-op" do
+            # Assert
             expect(@list.length).must_equal 0
+            # Act
             @list.delete(4)
+            # Assert
             expect(@list.length).must_equal 0
         end
 
         it "can delete valid values from list" do
+            # Arrange
             @list.add_last(9)
             @list.add_last(10)
             @list.add_first(4)
             @list.add_first(3)
             @list.add_first(2)
-
+            
+            # Act
             # delete fist node (requires updating head)
             @list.delete(2)
+            
+            # Assert
             expect(@list.get_first).must_equal 3
             expect(@list.length).must_equal 4
             expect(@list.get_last).must_equal 10
             expect(@list.find_max).must_equal 10
             expect(@list.find_min).must_equal 3
-
+            
+            # Act (again)
             # delete last node
             @list.delete(10)
+            # Assert
             expect(@list.get_first).must_equal 3
             expect(@list.length).must_equal 3
             expect(@list.get_last).must_equal 9
@@ -171,15 +195,18 @@ describe LinkedList do
 
     describe "nth_from_the_end" do
         it 'returns nil if n is outside the bounds of the list' do
+            # Act-Assert
             expect(@list.find_nth_from_end(3)).must_be_nil
         end
 
         it 'can retrieve an item at index n from the end in the list' do
+            # Arrange
             @list.add_first(1)
             @list.add_first(2)
             @list.add_first(3)
             @list.add_first(4)
 
+            # Act-Assert
             expect(@list.find_nth_from_end(0)).must_equal 1
             expect(@list.find_nth_from_end(1)).must_equal 2
             expect(@list.find_nth_from_end(2)).must_equal 3
@@ -190,12 +217,16 @@ describe LinkedList do
 
     describe "reverse" do
         it 'can retrieve an item at index n from the end in the list' do
+            # Arrange
             @list.add_first(4)
             @list.add_first(3)
             @list.add_first(2)
             @list.add_first(1)
+            
+            # Act
             @list.reverse
 
+            # Assert
             expect(@list.find_nth_from_end(0)).must_equal 1
             expect(@list.find_nth_from_end(1)).must_equal 2
             expect(@list.find_nth_from_end(2)).must_equal 3
